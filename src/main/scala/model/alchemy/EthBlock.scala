17239342b614a52e93422bf599e50b118d97f145
Sameer Brenn <sameer@creativedestruction.com>
2023-03-22 22:01:45 -0600

    fix refreshes

--- src/main/scala/model/alchemy/EthBlock.scala
+++ src/main/scala/model/alchemy/EthBlock.scala
@@ -1,10 +1,16 @@
 package model.alchemy
 
+import java.time.Instant
+
 case class EthBlock(
   number: String,
-  transactions: Seq[EthTransaction]) {
+  transactions: Seq[EthTransaction],
+  timestamp: String,
+                   ) {
+
+  val instant = Instant.ofEpochSecond(Integer.parseInt(timestamp.drop(2), 16))
   def toView(ethUsd: BigDecimal): String = {
-    s"<h1>$number</h1>" +
+    s"<h1>$number - $instant - $ethUsd</h1>" +
       transactions.map(_.toView(ethUsd)).mkString("<table><tr><th>From</th><th>To</th><th>ETH</th><th>USD</th><tr>", "</tr><tr>", "</tr></table>")
   }
 }
--- src/main/scala/server/Http.scala
+++ src/main/scala/server/Http.scala
@@ -1,15 +1,13 @@
 package server
 
 import cats.effect.IO
-import client.AlchemyClient
-import client.CoinMarketCapClient
+import client.{AlchemyClient, CoinMarketCapClient}
+import org.http4s.{Header, Headers, HttpRoutes, MediaType}
 import org.http4s.dsl.io._
 import org.http4s.headers.`Content-Type`
-import org.http4s.Header
-import org.http4s.Headers
-import org.http4s.HttpRoutes
-import org.http4s.MediaType
 import org.typelevel.ci.CIStringSyntax
+
+import java.time.Instant
 
 object Http {
   def makeRoutes(alchemyClient: AlchemyClient, cmcClient: CoinMarketCapClient): HttpRoutes[IO] = {
@@ -19,11 +17,14 @@ object Http {
           latestBlock <- alchemyClient.latestEthBlock
           ethUsd <- cmcClient.getEthUsd
           view = latestBlock.toView(ethUsd)
+          now <- IO(Instant.now.getEpochSecond)
+          sinceBlock = now - latestBlock.instant.getEpochSecond
+          refreshTime = Math.max(1, 12 - sinceBlock)
           response <- Ok(view)
           wType =
             response
               .withContentType(`Content-Type`(new MediaType("text", "html")))
-              .withHeaders(Headers(Seq(Header.Raw(ci"Refresh", "12"))))
+              .withHeaders(Headers(Seq(Header.Raw(ci"Refresh", refreshTime.toString))))
         } yield wType
 
     }
