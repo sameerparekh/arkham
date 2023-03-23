e7a85e8ba9b90f6a5061ede50e69542706050520
Sameer Brenn <sameer@creativedestruction.com>
2023-03-22 21:42:36 -0600

    cleanups

--- src/main/scala/model/alchemy/EthBlock.scala
+++ src/main/scala/model/alchemy/EthBlock.scala
@@ -5,6 +5,6 @@ case class EthBlock(
   transactions: Seq[EthTransaction]) {
   def toView(ethUsd: BigDecimal): String = {
     s"<h1>$number</h1>" +
-      transactions.map(_.toView(ethUsd)).mkString("<table><tr>", "</tr><tr>", "</tr></table>")
+      transactions.map(_.toView(ethUsd)).mkString("<table><tr><th>From</th><th>To</th><th>ETH</th><th>USD</th><tr>", "</tr><tr>", "</tr></table>")
   }
 }
